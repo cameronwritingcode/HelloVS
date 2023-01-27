beb65dc17b297f1e0ea59ab4c15d698854636ce9
cameronwritingcode <74562919+cameronwritingcode@users.noreply.github.com>
2023-01-27 01:28:45 -0800

    push 2

--- helloVS/src/App.java
+++ helloVS/src/App.java
@@ -1,5 +1,9 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, Big World!");
+
+        System.out.println("This is an exciting turn of events!");
+
+        System.out.println(" What will I make next? A Pokedex?" );
     }
 }
